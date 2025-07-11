55e483151312e6bb12668b22ed84244b8911597e
jadeilton <jadeiltondm@gmail.com>
2025-07-11 13:04:04 -0300

    Class Controller Test Create

--- src/test/java/com/example/magalu_jade/AgendamentoControllerTest.java
+++ src/test/java/com/example/magalu_jade/AgendamentoControllerTest.java
@@ -2,12 +2,17 @@ package com.example.magalu_jade;
 
 
 import com.example.magalu_jade.controller.AgendamentoController;
+import com.example.magalu_jade.dtos.AgendamentoRequestDTO;
+import com.example.magalu_jade.enuns.TipoComunicacao;
 import com.example.magalu_jade.service.AgendamentoService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
 
 @WebMvcTest(AgendamentoController.class)
 public class AgendamentoControllerTest {
@@ -28,11 +33,20 @@ public class AgendamentoControllerTest {
 
     // Esse mock vai ser injetado no controller pela Spring, simulando o comportamento esperado para testes.
     @MockitoBean
-    private AgendamentoService agendamentoService;
+    private AgendamentoService service;
+
+    @Test
+    void deveAgendamento() throws Exception {
 
 
+        AgendamentoRequestDTO requestDTO = new AgendamentoRequestDTO(
+                "jadeiltondm@gmail.com",
+                "Eu te amo",
+                TipoComunicacao.EMAIL,
+                LocalDateTime.now().plusMinutes(1)
+        );
+        
 
-
-
+    }
 
 }
