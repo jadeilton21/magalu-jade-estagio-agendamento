78f082bb803affc352c658fa79bb03112d8acc83
jadeilton <jadeiltondm@gmail.com>
2025-07-11 12:53:50 -0300

    Class Controller Test Create

---
+++ src/test/java/com/example/magalu_jade/AgendamentoControllerTest.java
@@ -0,0 +1,9 @@
+package com.example.magalu_jade;
+
+
+import com.example.magalu_jade.controller.AgendamentoController;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(AgendamentoController.class)
+public class AgendamentoControllerTest {
+}
